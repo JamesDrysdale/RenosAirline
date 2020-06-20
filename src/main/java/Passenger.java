3c768a1fca8c9b500f301198759f0e4a49b88a48
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-20 17:19:40 +0100

    Passengers can have bags or no bags

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,9 +1,19 @@
 public class Passenger {
     public String name;
+    public int bagCount;
 
     //constructor
-    public Passenger(String name){
-        this.name=name;
+    public Passenger(String name, int bagCount){
+        this.name = name;
+        this.bagCount = bagCount; // see comment
     }
+
+    /*  bagCount
+        I thought about using a class for bag, so you could make multiple
+        instances of bag. But since they all weigh the same, it didn't
+        seem necessary. The code could be refactored in the future if needed.
+
+        I am assuming each bag weighs 50 each
+     */
 
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -5,14 +5,27 @@ import static org.junit.Assert.assertEquals;
 
 public class PassengerTest {
     private Passenger passenger;
+    private Passenger passenger2;
 
     @Before
     public void before() {
-        passenger = new Passenger("Todd Manius");
+        passenger = new Passenger("Todd Manius", 2);
+        passenger2 = new Passenger("Nicu Icavoc", 0);
     }
 
     @Test
     public void passengerHasName(){
         assertEquals("Todd Manius", passenger.name);
     }
+
+    @Test
+    public void passengerCanHaveBags(){
+        assertEquals(2, passenger.bagCount);
+    }
+
+    @Test
+    public void passengerCanHaveNoBags(){
+        assertEquals(0, passenger2.bagCount);
+    }
+
 }
