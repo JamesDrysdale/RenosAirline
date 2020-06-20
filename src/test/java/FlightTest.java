85c719e2bc11372807b5ffd85d30eaf04ccfa54b
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-20 17:37:08 +0100

    Create test for empty list of booked passengers

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class FlightTest {
+    private ArrayList<PassengerList> bookedPassengers;
+
+    @Before
+    public void before(){
+        bookedPassengers = new PassengerList();
+    }
+
+    @Test
+    public void flightHasPassengerList__noBookedPassengers(){
+        assertEquals(0, bookedPassengers.count());
+    }
 }
